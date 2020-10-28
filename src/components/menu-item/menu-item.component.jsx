92880aeea48e8c60905b9fcc2cf82fa595d0839c
Ahmed Bizo <ahmedmajedb@hotmail.com>
2020-10-29 00:49:49 +0100

    update menu-items to navicate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,25 +2,28 @@
 
 import React from 'react';
 
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({titel, imageUrl}) => (
+const MenuItem = ({titel, imageUrl, history, LinkUrl, match}) => (
 
-    <div style={{
-        backgroundImage: 'url( '+imageUrl+')'
+    <div style={{backgroundImage: 'url( '+imageUrl+')'
  }}
-    className='menu-item'
-    
+     className='menu-item'
+    onClick = {() => history.push('+{match.url} + +{LinkUrl}+ ') }
     >
       
     <div className='content'>
 
        <h1 className='title'>{titel }</h1>
         <span className='subtitle'>SHOP NOW</span>
-        <span className='subtitle'> Rea 20% </span>
+        <span className='subtitle'> Rea </span>
+        <span className='subtitle'> 20% </span>
+
     </div>
 
 </div>
 );
             
-export default MenuItem;
+export default withRouter (MenuItem);
