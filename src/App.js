ec1a5a54562bd832486edfcbf53abb8e0144cc15
Ahmed Bizo <ahmedmajedb@hotmail.com>
2020-10-30 01:17:49 +0100

    adding headre component to my site

--- src/App.js
+++ src/App.js
@@ -7,21 +7,18 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-const HatsPage = () => (
+import ShopPage from './pages/shop/shop.component';
 
-  <div>
-    <h1>
-      HATS PAGE 
-    </h1>
-  </div>
-)
+import Header from './components/header/header.component.jsx';
+
 
 function App() {
   return (
   <div >
+    <Header/>
     <Switch>
     <Route exact path= '/' component = {HomePage} />
-    <Route path= '/hats' component = {HatsPage} />
+    <Route path= '/Shop' component = {ShopPage} />
     </Switch>
 
   </div>
