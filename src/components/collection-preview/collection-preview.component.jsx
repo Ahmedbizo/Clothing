d05079d6c5a7e5e3ee2d53d6f1db99f0c45019a6
Ahmed Bizo <ahmedmajedb@hotmail.com>
2020-11-10 23:59:14 +0100

    updated shop data with new data, updated shpo utils in to group items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,16 +6,16 @@ import './collection-preview.component.styles.scss';
 
 
 const CollectionPreview = ({ title, items }) => (
-    <div className='collection-preview'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <div className='preview'>
-        {items
-          .filter((item, idx) => idx < 4)
-          .map(({ id, ...otherItemProps }) => (
-            <CollectionItem key={id} {...otherItemProps} />
-          ))}
-      </div>
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
-  );
-  
-  export default CollectionPreview;
+  </div>
+);
+
+export default CollectionPreview;
